23eac5227c5990864e98f5ed16f10b18aac10022
tnrud430 <tnrud5666@gmail.com>
2023-04-09 19:02:46 +0900

    1st

---
+++ planner/src/main/java/com/planner/PlannerApplication.java
@@ -0,0 +1,14 @@
+package com.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerApplication.class, args);
+		//주석 하나만 달자
+	}
+
+}
